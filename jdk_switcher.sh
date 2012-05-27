9e390abe53783264b5f1b400c927216cc55c6caa
Michal Papis <mpapis@gmail.com>
2012-05-27 02:34:07 +0200

    compresing code

--- jdk_switcher.sh
+++ jdk_switcher.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-RETVAL=0
-
 if uname -a | grep x86_64 >/dev/null ; then
     ARCH_SUFFIX=amd64
 else
@@ -26,114 +24,47 @@ switch_to_openjdk6 () {
     echo "Switching to OpenJDK6 ($OPENJDK6_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK6_JAVA_HOME"
     sudo $UJA --set "$OPENJDK6_UJA_ALIAS"
     export JAVA_HOME="$OPENJDK6_JAVA_HOME"
-    RETVAL=0
 }
 
 switch_to_openjdk7 () {
     echo "Switching to OpenJDK7 ($OPENJDK7_UJA_ALIAS), JAVA_HOME will be set to $OPENJDK7_JAVA_HOME"
     sudo $UJA --set "$OPENJDK7_UJA_ALIAS"
     export JAVA_HOME="$OPENJDK7_JAVA_HOME"
-    RETVAL=0
 }
 
 switch_to_oraclejdk7 () {
     echo "Switching to Oracle JDK7 ($ORACLEJDK7_UJA_ALIAS), JAVA_HOME will be set to $ORACLEJDK7_JAVA_HOME"
     sudo $UJA --set "$ORACLEJDK7_UJA_ALIAS"
     export JAVA_HOME="$ORACLEJDK7_JAVA_HOME"
-    RETVAL=0
 }
 
 print_home_of_openjdk6 () {
     echo "$OPENJDK6_JAVA_HOME"
-    RETVAL=0
 }
 
 print_home_of_openjdk7 () {
     echo "$OPENJDK7_JAVA_HOME"
-    RETVAL=0
 }
 
 print_home_of_oraclejdk7 () {
     echo "$ORACLEJDK7_JAVA_HOME"
-    RETVAL=0
 }
 
 switch_to_sunjdk6 () {
     echo "Sun JDK 6 is not supported. It will be EOL in November 2012, consider moving on to OpenJDK 7 or Oracle JDK 7." >&2
-    RETVAL=1
 }
 
-switch_jdk () {
-    if echo "$JDK" | grep gcj > /dev/null ; then
-        echo "We do not support GCJ. I mean, come on. Are you Richard Stallman?" >&2;
-        exit 1;
-    fi
-
-    case "$JDK" in
-        openjdk6)
+switch_jdk()
+{
+    case "${1:-default}" in
+        *gcj*)
+            echo "We do not support GCJ. I mean, come on. Are you Richard Stallman?" >&2;
+            false
+            ;;
+        openjdk6|openjdk1.6|openjdk1.6.0|jdk6|1.6.0|1.6|6.0)
             switch_to_openjdk6
             ;;
-        openjdk1.6)
-            switch_to_openjdk6
-            ;;
-        openjdk1.6.0)
-            switch_to_openjdk6
-            ;;
-        jdk6)
-            switch_to_openjdk6
-            ;;
-        1.6.0)
-            switch_to_openjdk6
-            ;;
-        1.6)
-            switch_to_openjdk6
-            ;;
-        6.0)
-            switch_to_openjdk6
-            ;;
-        openjdk7)
-            switch_to_openjdk7
-            ;;
-        jdk7)
-            switch_to_openjdk7
-            ;;
-        1.7.0)
-            switch_to_openjdk7
-            ;;
-        1.7)
-            switch_to_openjdk7
-            ;;
-        7.0)
-            switch_to_openjdk7
-            ;;
-        oraclejdk7)
-            switch_to_oraclejdk7
-            ;;
-        oraclejdk1.7)
-            switch_to_oraclejdk7
-            ;;
-        oraclejdk1.7.0)
-            switch_to_oraclejdk7
-            ;;
-        oracle7)
-            switch_to_oraclejdk7
-            ;;
-        oracle1.7.0)
-            switch_to_oraclejdk7
-            ;;
-        oracle7.0)
-            switch_to_oraclejdk7
-            ;;
-        oracle)
-            switch_to_oraclejdk7
-            ;;
-        sunjdk7)
-            switch_to_oraclejdk7
-            ;;
-        sun7)
-            switch_to_oraclejdk7
-            ;;
-        sun)
+        openjdk7|jdk7|1.7.0|1.7|7.0|oraclejdk7|oraclejdk1.7|oraclejdk1.7.0|oracle7|oracle1.7.0|oracle7.0|oracle|sunjdk7|sun7|sun)
             switch_to_oraclejdk7
             ;;
         default)
@@ -146,7 +77,11 @@ switch_jdk () {
 }
 
 
-print_java_home () {
+print_java_home()
+{
+    typeset JDK
+    JDK="$1"
+
     if echo "$JDK" | grep gcj > /dev/null ; then
         echo "We do not support GCJ. I mean, come on. Are you Richard Stallman?" >&2;
         exit 1;
@@ -226,23 +161,24 @@ print_java_home () {
 }
 
 
-jdk_switcher () {
+jdk_switcher()
+{
+    typeset COMMAND JDK 
     COMMAND="$1"
     JDK="$2"
 
     case "$COMMAND" in
         use)
-            switch_jdk
+            switch_jdk "$JDK"
             ;;
         home)
-            print_java_home
+            print_java_home "$JDK"
             ;;
         *)
             echo "Usage: $0 {use|home} [ JDK version ]" >&2
-            RETVAL=1
+            false
             ;;
     esac
 
-
-    return $RETVAL
+    return $?
 }
